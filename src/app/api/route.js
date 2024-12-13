df52b4bbd4b0654d79d57bbdd30d7166d80618b2
Arturo Pérez <arjperez31@gmail.com>
2024-12-13 00:11:57 -0400

    adding postgresConnection to the project

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -2,13 +2,15 @@ import { NextResponse } from "next/server";
 import pool from "@/utils/postgresConnection";
 
 export const GET = async () => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
     const res = await client.query("select * from usuarios");
     const users = res.rows;
 
     return NextResponse.json(users);
   } catch (error) {
     return NextResponse.json(error);
+  } finally {
+    client.release();
   }
 };
