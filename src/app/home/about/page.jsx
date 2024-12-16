a3c80cce40817842cb058b773149fe7159b1a7e7
Arturo Pérez <arjperez31@gmail.com>
2024-12-15 21:09:39 -0400

    adding homePage and contact

--- src/app/home/about/page.jsx
+++ src/app/home/about/page.jsx
@@ -4,16 +4,16 @@ import { Image } from "@nextui-org/react";
 
 export default function AboutPage() {
   return (
-    <div className="container mx-auto">
-      <section className="bg-slate-600 p-6 my-10 rounded ">
+    <div className="container mx-auto mt-10 mb-10">
+      <section className="bg-zinc-900 p-6 rounded-lg ">
         <h1 className="text-4xl font-bold text-white mb-4">Sobre Nosotros</h1>
         <p className="text-medium text-slate-300 mb-2">
-          <strong className="text-white">UcabAir</strong>, es una empresa
+          <strong className="text-white">UCABAIR</strong>, es una empresa
           aeronáutica y el mayor fabricante de aviones de Venezuela. Su sede
           central se encuentra en la ciudad de Catia la Mar, Estado La Guaira.
           Las fábricas más grandes están situadas en los alrededores de la
           ciudad de Valencia Estado Carabobo, Maracay Estado Aragua, Guatire
-          Estado Miranda. UcabAir es la compañía líder nacional y principal
+          Estado Miranda. UCABAIR es la compañía líder nacional y principal
           proveedor de servicios de la nación en el diseño y fabricación de
           helicópteros, sistemas electrónicos y sistemas avanzados de
           comunicación e información. También proporciona numerosos servicios de
@@ -21,13 +21,13 @@ export default function AboutPage() {
           es uno de los mayores exportadores de Venezuela en términos de ventas.
         </p>
         <p className="text-medium text-slate-300 mb-2">
-          UcabAir introdujo en 1980 el primer avión comercial de reacción en
+        UCABAIR introdujo en 1980 el primer avión comercial de reacción en
           Venezuela. Previamente ya habían aparecido dos aviones de este tipo en
           Europa: el de Havilland Comet fabricado en el Reino Unido, y el
-          Caravelle, fabricado en Francia. Con el nuevo avión, el AU-80, UcabAir
+          Caravelle, fabricado en Francia. Con el nuevo avión, el AU-80, UCABAIR
           se convirtió en el líder de los fabricantes de reactores para
           pasajeros. Se trataba de un avión cuatrimotor con capacidad para 120
-          pasajeros y 7 destinado a rutas de largo alcance. Poco después UcabAir
+          pasajeros y 7 destinado a rutas de largo alcance. Poco después UCABAIR
           desarrolló una segunda versión de este avión, el AU-801, para rutas
           menos largas, y unos años más tarde apareció el AU-802, un avión de
           capacidad similar, pero dotado de tres reactores, y concebido para
@@ -44,8 +44,8 @@ export default function AboutPage() {
           continuas mejoras tecnológicas. También han ido apareciendo variantes
           adicionales, por lo general versiones más largas para una mayor
           capacidad de pasajeros, incluso existe un AU-87A de negocios para
-          grandes empresas el UcabAir Business Plane. Al comenzar la década de
-          los 90 UcabAir fabrica el primer ejemplar del nuevo avión AU-747Plus,
+          grandes empresas el UCABAIR Business Plane. Al comenzar la década de
+          los 90 UCABAIR fabrica el primer ejemplar del nuevo avión AU-747Plus,
           fue puesto en servicio, un cuatrimotor de largo alcance con capacidad
           para 460 personas, y con ello el mayor avión comercial de la historia
           de la aviación en Venezuela. Este avión ha tenido un éxito
@@ -55,11 +55,11 @@ export default function AboutPage() {
           avión de dos pisos con capacidad de asientos superior al AU-747Plus se
           encuentra por primera vez con un competidor para este avión, después
           de su lanzamiento. En 1993 la situación económica de la empresa era
-          muy estable, y con ello UcabAir entregó el ejemplar número 1.000 de su
+          muy estable, y con ello UCABAIR entregó el ejemplar número 1.000 de su
           AU-747Plus. A medida que el tráfico de pasajeros iba en aumento en
           todo el mundo la competencia entre los fabricantes de aviones se
-          endurecía. UcabAir tuvo que enfrentarse también a un recién llegado,
-          esta vez de Europa, el consorcio 8 Airbus. Ello obligó a UcabAir a
+          endurecía. UCABAIR tuvo que enfrentarse también a un recién llegado,
+          esta vez de Europa, el consorcio 8 Airbus. Ello obligó a UCABAIR a
           desarrollar a su vez nuevos aviones, que fueron el AU-747Platinium,
           avión de un pasillo central para recorridos medios, el AU-747Gold, de
           cabina ancha y dos pasillos, para rutas medias y largas, y con
---
+++ src/utils/avionesJson.js
@@ -0,0 +1,26 @@
+export const carouselImages = [
+  {
+    id: 1,
+    width: 100,
+    height: 50,
+    url: "/images/avion1.png",
+  },
+  {
+    id: 2,
+    width: 100,
+    height: 50,
+    url: "/images/avion2.png",
+  },
+  {
+    id: 3,
+    width: 100,
+    height: 50,
+    url: "/images/avion3.png",
+  },
+  {
+    id: 4,
+    width: 100,
+    height: 50,
+    url: "/images/avion4.png",
+  },
+];
